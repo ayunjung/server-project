d1c0708f63c851f1ba9a97470c27a7d40e93f385
ayunjung <dkdbs1114@naver.com>
2022-03-18 00:08:20 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,36 +1,10 @@
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import React from 'react'
-import LoginModal from './components/LoginModal';
+import Searchbar from './components/Searchbar';
 
 
 function App() {
     return (
-        <LoginModal />
+        <Searchbar />
     );
 }
 
