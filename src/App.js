e0d5d21e4c622546c3cd916d35da08dea6aa724e
ayunjung <dkdbs1114@naver.com>
2022-03-17 20:39:14 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,110 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+import React from 'react'
+import styled from 'styled-components'
+import { Formik } from 'formik'
+
+const Login = () => {
+
+    const Loginform = styled.div`
+      width: 600px;
+      font-size: 25px;
+    `
+
+    const Userinput = styled.input`
+      margin: 10px 0;
+      width: 100%;
+      height: 70px;
+      border: 1px solid #999;
+      border-radius: 5px;
+      font-size: 30px;
+    `
+
+    const Checkbox = styled.input`
+      margin: 3px;
+    `
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Formik>
+      <Loginform>
+        <Userinput placeholder='Username' />
+        <Userinput placeholder='password' />
+        <Checkbox type="checkbox" id="checkbox" /><label for="checkbox">로그인 유지</label>
+      </Loginform>
+    </Formik>
+  )
 }
 
-export default App;
+export default Login
+
+
+// import styled from 'styled-components';
+// import React from 'react'
+
+// const magnifyStyle = {
+//     marginTop : '9',
+//     marginLeft : '10',
+//     marginRight : '10',
+//     height : '30',
+//   }
+
+//   const Manidiv = styled.div`
+//     background : #cacaca;
+//     height : 100%;
+//     width : 50px;
+//     border-radius : 5px 0 0 5px;
+//   `
+
+//   const SMainSearchbar = styled.div`
+//     width : 373px;
+//     height : 50px;
+//     border-radius : 5px;
+//     border : 1px solid gray;
+//     margin : 5px;
+//     display : flex;
+//   `
+
+//   const Input = styled.input`
+//     width : 320px;
+//     font-size : 20px;
+//     padding : 7px;
+//   `
+
+// const MainSearchbar = () => {
+//   return (
+//     <div>
+//         <SMainSearchbar>
+//             <Manidiv>
+//             </Manidiv>
+//             <Input></Input>
+//         </SMainSearchbar>
+//     </div>
+//   )
+// }
+
+// export default MainSearchbar
