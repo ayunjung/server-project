bf6cc312f664bf6a1cf711b7c47addecf3949144
ayunjung <dkdbs1114@naver.com>
2022-04-06 00:07:41 +0900

    0405 all commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import LoginPage from './file/LoginPage';
+import RegisterPage from './file/RegisterPage';
+import Mypage from './file/Mypage';
+import LectureView from './file/LectureView';
+import LectureVidio from './file/LectureVidio';
+import MainPage from './file/MainPage';
+import Question from './file/Question';
+import CommunityPage from './file/CommunityPage';
+import LectureList from './file/LectureList';
 
 
 function App() {
     return (
-        <LoginPage />
+        <Router>
+            <Switch>
+                <Route exact path="/LectureList"><LectureList/></Route>
+                <Route exact path="/CommunityPage"><CommunityPage/></Route>
+                <Route exact path="/Question"><Question/></Route>
+                <Route exact path="/MainPage"><MainPage/></Route>
+                <Route exact path="/LectureVidio"><LectureVidio/></Route>
+                <Route exact path="/LectureView"><LectureView/></Route>
+                <Route exact path="/LoginPage"><LoginPage/></Route>
+                <Route exact path="/Mypage"><Mypage/></Route>
+                <Route exact path="/RegisterPage"><RegisterPage/></Route>
+                <Route exact path="/"><LectureList/></Route>
+            </Switch>
+        </Router>
     );
 }
 
